84ba538bbdd70d59aa8dcb2b0374e3a63c570b84
Satjatum <pooklnw.2456@gmail.com>
2025-12-09 21:28:23 +0700

    แก้ไข supabase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:manager_room_project/views/splash_ui.dart';
-import 'config/superbase_config.dart';
+// import 'config/superbase_config.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Link Supabase Setupp
-  await SupabaseConfig.initialize();
+  await Supabase.initialize(
+    url: 'https://hhbqmrtpvqdmkscagkqi.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhoYnFtcnRwdnFkbWtzY2Fna3FpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3MjAwODIsImV4cCI6MjA3MTI5NjA4Mn0.nfWJ3MCf5PyVw-Bf4ztauSS9vCD7UViVLZmAg6ilkHc',
+  );
 
   runApp(const ManagerRoomProject());
 }
